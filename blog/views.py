b7007a3b5452e928e16514e615bf673ec8898715
Vinicius Farina <vsfarina@usp.br>
2024-11-04 16:02:45 -0300

    Commit das views com Forms

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect 
 from .models import Post
+from .forms import PostForm
 
 def post_list(request):
     posts = Post.objects.all() 
@@ -9,16 +10,21 @@ def post_detail(request, pk):
     return render(request, 'blog/post_detail.html', {'post': post})
 def post_new(request):
     if request.method == 'POST':
-        title = request.POST['title']
-        content = request.POST['content']
-        Post.objects.create(title=title, content=content)
-        return redirect('post_list')
-    return render(request, 'blog/post_form.html')
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('post_list')
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_form.html', {'form': form})
     
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == 'POST':
-        post.title = request.POST['title']
-        post.content = request.POST['content']
-        post.save() return redirect('post_detail', pk=pk)
-    return render(request,'blog/post_confirm_delete.html', {'post': post})
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('post_detail', pk=pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_form.html', {'form': form})
