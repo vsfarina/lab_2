f4b7e2aaa2bf4e2bc61c1b77a26d03ba86de2e35
Vinicius Farina <vsfarina@usp.br>
2024-11-16 14:33:08 -0300

    usuario anonimo implementado de novo5

--- blog/views.py
+++ blog/views.py
@@ -35,7 +35,7 @@ def post_new(request):
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
-    return render(request, 'blog/post_form.html', {'form': form})
+    return render(request, 'blog/post_form2.html', {'form': form})
 
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
