e6a66660d9ca53cba58b6406735a85648fa4226f
Vinicius Farina <vsfarina@usp.br>
2024-11-16 14:28:12 -0300

    usuario anonimo implementado de novo4

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import post_list, post_detail, post_new, post_edit, post_delete_confirm, add_comment
+from .views import post_list, post_detail, post_new, post_edit, post_delete_confirm
 from . import views
 
 urlpatterns = [
@@ -7,7 +7,6 @@ urlpatterns = [
     path('post/<int:pk>/', post_detail, name='post_detail'),  # Certifique-se de que essa URL está correta
     path('post/new/', post_new, name='post_new'),
     path('post/<int:pk>/edit/', post_edit, name='post_edit'),
-    path('post/<int:post_pk>/comment/', add_comment, name='add_comment'),
     path('categories/', views.category_list, name='category_list'),
     path('category/<int:category_id>/', views.category_detail, name='category_detail'),
     path('post/<int:pk>/delete/confirm/', views.post_delete_confirm, name='post_delete_confirm'),
